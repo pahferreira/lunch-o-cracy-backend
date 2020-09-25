64474cbdbd4c3c9b5255a76505e91c536bcad060
Pah <pah.dsf@gmail.com>
2020-09-25 18:02:49 -0300

    project architecture and lib setups

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+}
---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'lunch-o-cracy',
+      user: 'postgres',
+      password: 'postgres',
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
+}
